26b04c53bde6032515722ed5af2df1c4db77e43b
IraMira333 <irinat333@gmail.com>
2025-08-26 02:30:19 +0300

    Added Article section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { AdvantagesList } from "@/components/advantages/AdvantagesList";
+import { Article } from "@/components/article/Article";
 import { CTA } from "@/components/CTA/CTA";
-import { ctaDiscount } from "@/components/data/ctaContent";
+import { article } from "@/components/data/article";
+import { ctaDiscount, ctaTrial } from "@/components/data/ctaContent";
 import { DetailsList } from "@/components/details/DetailsList";
 import { Hero } from "@/components/hero/Hero";
 
@@ -11,6 +13,8 @@ export default function Home() {
             <AdvantagesList />
             <DetailsList />
             <CTA cta={ctaDiscount} />
+            <Article article={article} />
+            <CTA cta={ctaTrial} />
         </>
     );
 }
---
+++ src/components/article/Article.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+export const Article = ({
+    article,
+}: {
+    article: { title: string; text: string[] };
+}) => {
+    return (
+        <section
+            id="about"
+            className="pc:py-16 pc:px-10 pc:flex items-end justify-between overflow-hidden px-4 py-12"
+        >
+            <article className="pc:w-[50%] pc:mb-0 mb-6">
+                <h2 className="font-montserrat mb-6 max-w-[300px] text-4xl font-semibold">
+                    {article.title}
+                </h2>
+                {article.text.map((paragraph, index) => (
+                    <p
+                        className="pc:mx-0 mx-auto mb-4 max-w-[700px] whitespace-pre-line last:mb-0"
+                        key={index}
+                    >
+                        {paragraph}
+                    </p>
+                ))}
+            </article>
+            <div className="pc:w-[40%] pc:max-w-full pc:mx-0 mx-auto max-w-[330px]">
+                <Image
+                    src="/article.png"
+                    alt="article"
+                    width={1440}
+                    height={960}
+                    className="w-full"
+                />
+            </div>
+        </section>
+    );
+};
