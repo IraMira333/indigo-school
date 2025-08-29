da92924e97ca6206e0530b6069f4db41c367e561
IraMira333 <irinat333@gmail.com>
2025-08-29 09:20:39 +0300

    Update route.ts

--- src/app/api/application/route.ts
+++ src/app/api/application/route.ts
@@ -10,34 +10,33 @@ const SPREADSHEET_ID = process.env.NEXT_PUBLIC_APPLICATION_SPREADSHEET_ID || "";
 const RANGE = "Аркуш1!A2:G2";
 
 export async function POST(request: NextRequest) {
-    const { name, phone, age, language, online, group } = await request.json();
-    const date = new Date().toLocaleString();
+    try {
+        const { name, phone, age, language, online, group } =
+            await request.json();
+        const date = new Date().toLocaleString();
 
-    if (request.method === "POST") {
-        try {
-            await sendDataToGoogleSheet(SPREADSHEET_ID, RANGE, [
-                name,
-                phone,
-                age,
-                language,
-                online,
-                group,
-                date,
-            ]);
-            await sendTelegramNotification(
-                `📩 Нова заявка!\n\n👤 Ім'я: ${name}\n📞 Телефон: ${phone}\n🎂 Вік: ${age}\n📚 Мова: ${language}\n📍 Формат: ${online}\n👥 Тип: ${group}\n🕒 Час: ${date}`
-            );
+        await sendDataToGoogleSheet(SPREADSHEET_ID, RANGE, [
+            name,
+            phone,
+            age,
+            language,
+            online,
+            group,
+            date,
+        ]);
+        await sendTelegramNotification(
+            `📩 Нова заявка!\n\n👤 Ім'я: ${name}\n📞 Телефон: ${phone}\n🎂 Вік: ${age}\n📚 Мова: ${language}\n📍 Формат: ${online}\n👥 Тип: ${group}\n🕒 Час: ${date}`
+        );
 
-            return NextResponse.json({ message: "Data appended successfully" });
-        } catch (error) {
-            console.error(
-                "🚨 Google Sheets API error:",
-                JSON.stringify(error, null, 2)
-            );
-            return NextResponse.json(
-                { error: "Failed to append data to the sheet", details: error },
-                { status: 500 }
-            );
-        }
+        return NextResponse.json({ message: "Data appended successfully" });
+    } catch (error) {
+        console.error(
+            "🚨 Google Sheets API error:",
+            JSON.stringify(error, null, 2)
+        );
+        return NextResponse.json(
+            { error: "Failed to append data to the sheet", details: error },
+            { status: 500 }
+        );
     }
 }
